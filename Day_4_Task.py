a8836df0d8bc428c29e2b5d9e8748c6bd3da974a
RubeshU <85781136+RubeshU@users.noreply.github.com>
2021-06-15 14:33:14 +0530

    Add files via upload

---
+++ Day_4_Task.py
@@ -0,0 +1,20 @@
+a=b=c=100
+print(a/10)
+print(a/50)
+print(a/60)
+s = 'Rubesh'
+print(s)
+print(s.replace('b','G'))
+a=10
+b=5.7
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+a=float(a)
+b=int(b)
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
