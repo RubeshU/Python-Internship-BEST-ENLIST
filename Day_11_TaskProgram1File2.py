e42263b79b3465e3129cde065cf20e6b226560a8
RubeshU <85781136+RubeshU@users.noreply.github.com>
2021-06-23 20:06:55 +0530

    Add files via upload

---
+++ Day_11_TaskProgram1File2.py
@@ -0,0 +1,7 @@
+import Day_11_TaskProgram1File1 as obj
+
+print('list present in the module Day_11_TaskProgram1File1 : ',obj.list1)
+
+obj.list1[2]=0
+
+print('After changing the 3rd value in the list present in the module : ',obj.list1)
---
+++ Day_11_TaskProgram2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+dict1 = {
+    'products': ['Watch','Earphone','Pendrive'],
+    'price':[1000,700,1200]
+}
+
+var1 = pd.DataFrame(dict1)
+
+print(var1)
