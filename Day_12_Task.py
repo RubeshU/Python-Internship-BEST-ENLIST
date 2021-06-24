457af89a0122ae1802a98738843caa746fb1975f
RubeshU <85781136+RubeshU@users.noreply.github.com>
2021-06-24 23:06:23 +0530

    Add files via upload

---
+++ Day_12_Task.py
@@ -0,0 +1,8 @@
+file1=open("30 days 30 hour operations.txt","w")
+file1.write('I have completed 10 days successfully ')
+file1.close()
+file1=open("30 days 30 hour operations.txt","a")
+n=input("Enter your name:")
+file1.write(n)
+print("Name was appended successfully")
+file1.close()
