c9182efd1234b830887cc75a37ff2d0f71d8bf92
RubeshU <85781136+RubeshU@users.noreply.github.com>
2021-07-11 20:03:00 +0530

    Add files via upload

---
+++ Day_26_Task.py
@@ -0,0 +1,5 @@
+list1= ['rubesh','balaji','gokul','vignesh','shreenath']
+print(list(enumerate(list1)))
+print(enumerate(list1))
+tuple1=('rubesh','balaji','gokul','vignesh','shreenath')
+print(tuple(enumerate(tuple1)))
